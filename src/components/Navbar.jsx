7e27a5b1782dc0496b1116efd2a820be66bedc21
Elilaw179 <sundayelisha179@gmail.com>
2025-05-14 08:35:53 +0100

    Adding google ads sense snippet code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,5 @@
-// import React from 'react';
-
-// const Navbar = () => {
-//   return (
-//     <nav className="bg-black text-white flex justify-between items-center px-6 py-4 fixed w-full z-50">
-//       <h1 className="text-2xl font-bold">🎸 PrinceJ</h1>
-//       <ul className="flex space-x-6 text-lg">
-//         <li><a href="#home" className="hover:text-yellow-400">Home</a></li>
-//         <li><a href="#about" className="hover:text-yellow-400">About</a></li>
-//         <li><a href="#music" className="hover:text-yellow-400">Music</a></li>
-//         <li><a href="#gallery" className="hover:text-yellow-400">Gallery</a></li>
-//         <li><a href="#contact" className="hover:text-yellow-400">Contact</a></li>
-//       </ul>
-
-      
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
 
 
-
-
-
-
-
-
-// src/components/Navbar.jsx
 import React, { useState } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 
